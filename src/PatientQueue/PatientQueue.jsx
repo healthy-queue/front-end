63160362907ef1ef7cc4d34d7ee97e31bdcd61d0
MuckT <mcgtom10@gmail.com>
2022-03-05 21:24:49 -0800

    style: remove unused import

--- src/PatientQueue/PatientQueue.jsx
+++ src/PatientQueue/PatientQueue.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { setActivePatient } from './reducer'
-const Data = require('../data/sample-queue.json')
 
 const PatientQueue = () => {
   const queueData = useSelector(state => state.patientQueue.queue)
