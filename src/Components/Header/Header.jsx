7a406afd6d9cde868d73ab998077461ab2090e99
jeremywbrazell <jwbrazell@gmail.com>
2022-03-05 19:11:32 -0800

    pushing started header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "../Login/LoginButton";
 import LogoutButton from "../Logout/LogoutButton";
-
+import ResponsiveAppBar from './AppBar';
 
 const Header = () => {
   const { isAuthenticated } = useAuth0();
   return (
     <div>
+      <ResponsiveAppBar />
       <p>Healthy Queue</p>
         { isAuthenticated
           ? <LogoutButton />
@@ -17,3 +18,4 @@ const Header = () => {
 }
 
 export default Header;
+
