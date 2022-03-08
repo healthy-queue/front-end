6addaa4f4e7f6a916ce9ee6092c5932f8c0973bb
MuckT <mcgtom10@gmail.com>
2022-03-07 23:14:32 -0800

    style: adjust app scss

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
-import Header from '../Components/Header/Header';
+import Header from '../Header/Header';
 import MyRouter from '../Router/Router';
+import './App.scss';
 
 function App() {
   return (
