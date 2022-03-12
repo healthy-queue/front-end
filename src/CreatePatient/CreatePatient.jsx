2350dd480997ef7792caf17e1303b752b3fa1d80
MuckT <mcgtom10@gmail.com>
2022-03-11 22:53:14 -0800

    feat: add patients to the database

--- src/CreatePatient/CreatePatient.jsx
+++ src/CreatePatient/CreatePatient.jsx
@@ -1,14 +1,20 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+import { pickBy } from 'lodash';
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import Input from '@mui/material/Input'
-import Button from '@mui/material/Button';
-import { useAuth0 } from "@auth0/auth0-react";
-import './CreatePatient.scss'
+import './CreatePatient.scss';
+const axios = require('axios');
 
 const CreatePatient = () => {
   const { isAuthenticated, user } = useAuth0();
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+
+  const onSubmit = async data => {
+    const sanitizedValues = pickBy(data, value => value !== "");
+    await axios.post(`${process.env.REACT_APP_API}/patient`, sanitizedValues);
+  }
 
   // TODO: lets see about using roles more better - Set user roles in Auth0
   // https://manage.auth0.com/dashboard/us/<Auth0 Domain>/users
@@ -20,14 +26,14 @@ const CreatePatient = () => {
           <h2> Add Patient Info </h2>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="input-items">
-              <Input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
-              <Input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
-              <Input type="datetime" placeholder="Date of Birth" {...register("Date of Birth", {require: true})} />
-              <Input type="tel" placeholder="Phone number" {...register("Phone number", { required: false, maxLength: 12})} />
-              <Input type="text" placeholder="Email" {...register("Email", { required: false, pattern: /^\S+@\S+$/i})} />
+              <Input type="text" placeholder="First name" {...register("first_name", { required: true, maxLength: 80 })} />
+              <Input type="text" placeholder="Last name" {...register("last_name", {required: true, maxLength: 100})} />
+              <Input type="datetime" placeholder="Date of Birth" {...register("date_of_birth", {require: true})} />
+              <Input type="tel" placeholder="Phone number" {...register("phone_number", { required: false, maxLength: 12})} />
+              <Input type="text" placeholder="Email" {...register("email_address", { required: false, pattern: /^\S+@\S+$/i})} />
 
-              <Input type="text" placeholder="Insurance Carrier" {...register("Insurance Carrier", {})} />
-              <Input type="text" placeholder="Insurance Group" {...register("Insurance Group", {})} />
+              <Input type="text" placeholder="Insurance Carrier" {...register("insurance_carrier", {})} />
+              <Input type="text" placeholder="Insurance Group" {...register("insurance_group", {})} />
             </div>
             <div className="submit-button">
               <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
