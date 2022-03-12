c757869b61feb8632bfa4241d80b58431933377d
antoni <antoni.dev0@gmail.com>
2022-03-11 20:28:40 -0800

    rms DataTable from App component and comment out socektio logic + imports

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,20 +1,19 @@
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 import Header from '../Header/Header';
 import MyRouter from '../Router/Router';
-import io from 'socket.io-client'
-import DataTable from '../Home/Table';
+// import io from 'socket.io-client'
 import './App.scss';
 
 function App() {
 
-  const socket = io.connect('http://localhost:8000')
-  useEffect(() => {
-    socket.on("new-message", payload => console.log('server payload: ', payload))
-  }, [])
+  // const socket = io.connect('http://localhost:8000')
+  // useEffect(() => {
+  //   socket.on("new-message", payload => console.log('server payload: ', payload))
+  // }, [])
+
   return (
     <div className="App">
       <Header />
-      <DataTable />
       <MyRouter />
     </div>
   );
