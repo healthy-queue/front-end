272e5ba4e63e62c91a45786a68db03fc616d18a5
MuckT <mcgtom10@gmail.com>
2022-03-12 19:35:35 -0800

    fix: initial load of data for patients and queues after login

--- src/Patients/PatientTable.jsx
+++ src/Patients/PatientTable.jsx
@@ -1,13 +1,27 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import Grid from '@material-ui/core/Grid';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import DataTable from '../Components/DataTable';
 import CreatePatientModal from './CreatePatientModal';
+import { setPatients } from './reducer';
+const axios = require('axios');
 
 
 const PatientTable = () => {
+  const dispatch = useDispatch()
   const { isAuthenticated, user } = useAuth0();
   const userRole = user && user['http://localhost:3000/role'] && user['http://localhost:3000/role'][0] || 'general'
+
+  // Get Patients on initial load
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(`${process.env.REACT_APP_API}/patients`)
+      console.log(result.data)
+      dispatch(setPatients(result.data))
+    }
+    fetchData().catch(console.error)
+  }, [])
 
   return (
     isAuthenticated && userRole === 'triage' || userRole === 'provider'
--- src/Queue/QueueContainer.jsx
+++ src/Queue/QueueContainer.jsx
@@ -1,11 +1,27 @@
-import Container from '@material-ui/core/Container';
-import QueueTabs from './QueueTabs';
 import { useAuth0 } from '@auth0/auth0-react';
+import Container from '@material-ui/core/Container';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import QueueTabs from './QueueTabs';
+import { setQueue } from './reducer';
+const axios = require('axios');
+
 
 const QueueContainer = () => {
-
+  const dispatch = useDispatch()
   const { isAuthenticated, user } = useAuth0();
   const userRole = user && user['http://localhost:3000/role'] && user['http://localhost:3000/role'][0] || 'general'
+
+  // Get Queue on initial load
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(`${process.env.REACT_APP_API}/queue`)
+      console.log(result.data)
+      dispatch(setQueue(result.data))
+    }
+    fetchData().catch(console.error)
+  }, [])
+  
   return (
     isAuthenticated && userRole === 'triage' || userRole === 'provider'
       ? <>
